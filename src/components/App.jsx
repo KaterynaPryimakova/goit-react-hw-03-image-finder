b8a4e3270a35172fb6c5552f73960b4dbe1e7b0a
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2023-12-19 18:01:38 +0200

    start working of fetch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,29 @@ import { Loader } from './Loader/Loader';
 import { fetchImagesWithQuery } from 'api/api';
 
 export class App extends Component {
-  async componentDidMount() {}
+  state = {
+    searchQuery: '',
+    gallery: [],
+    isLoading: false,
+    error: null,
+  };
+
+  async componentDidMount() {
+    const gallery = await fetchImagesWithQuery(this.state.searchQuery);
+    this.setState({ gallery });
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      searchQuery: e.target.value,
+    });
+  };
 
   render() {
     return (
       <div>
-        <Searchbar></Searchbar>
+        <Searchbar onSubmit={this.handleSubmit}></Searchbar>
         <Loader />
         <ImageGallery searchResult={[]} />
       </div>
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,35 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { SearchHeader, Form, SearchButton, Input } from './Searchbar.styled';
 import { ReactComponent as SearchIcon } from '../../icons/search.svg';
 
-export class Searchbar extends Component {
-  state = {
-    searchQuery: '',
-  };
+export const Searchbar = onSubmit => {
+  return (
+    <SearchHeader>
+      <Form onSubmit={onSubmit}>
+        <SearchButton type="submit">
+          <SearchIcon />
+        </SearchButton>
 
-  handleChange = e => {
-    this.setState({
-      searchQuery: e.target.value,
-    });
-  };
-  render() {
-    return (
-      <SearchHeader>
-        <Form onSubmit={this.onSubmit}>
-          <SearchButton type="submit">
-            <SearchIcon fill="" />
-          </SearchButton>
-
-          <Input
-            value={this.state.searchQuery}
-            onChange={this.handleChange}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </Form>
-      </SearchHeader>
-    );
-  }
-}
+        <Input
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </Form>
+    </SearchHeader>
+  );
+};
